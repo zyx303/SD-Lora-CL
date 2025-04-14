f5c67a96a3cc25ba6807855a3ba82e929b3c25e9
Yichen WU <55070269+WuYichen-97@users.noreply.github.com>
2025-04-14 07:54:58 -0400

    run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python3 main.py --config=./exps/finetune_inr.json  >> InR.log 2>&1 &
-python3 main.py --config=./exps/finetune_ina.json  >> InA.log 2>&1 &
+python3 main.py --config=./exps/sdlora_inr.json  >> InR.log 2>&1 &
+python3 main.py --config=./exps/sdlora_ina.json  >> InA.log 2>&1 &
