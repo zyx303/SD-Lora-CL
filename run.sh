3b340313e66b8e8be922f2024b31030e0991feb4
zyx303 <2091183827@qq.com>
2025-09-20 00:06:23 +0800

    store

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+python3 main.py --config=./exps/sdlora_c100.json
 python3 main.py --config=./exps/sdlora_inr.json  >> InR.log 2>&1 &
 python3 main.py --config=./exps/sdlora_ina.json  >> InA.log 2>&1 &
