d852d7ad6b2c57e9152fe18c84e3c04ce0597ba5
zyx303 <2091183827@qq.com>
2026-01-26 22:44:47 +0800

    scrpit_c100

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-python3 main.py --config=./exps/sdlora_inr.json  >> InR.log 2>&1 &
-python3 main.py --config=./exps/sdlora_ina.json  >> InA.log 2>&1 &
+export CUDA_VISIBLE_DEVICES=6,7
+python3 main.py --config=./exps/sdlora_c100.json  > C100.log 2>&1
+
+# python3 main.py --config=./exps/sdlora_inr.json  >> InR.log 2>&1 &
+# python3 main.py --config=./exps/sdlora_ina.json  >> InA.log 2>&1 &
