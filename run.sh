f0e41e20835e18134e65e349af172d429e494af4
zyx303 <2091183827@qq.com>
2026-01-27 02:06:58 +0800

    多卡

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-export CUDA_VISIBLE_DEVICES=6,7
+# 设置 GPU 设备
+export CUDA_VISIBLE_DEVICES=4,5
+
+# 设置 Hugging Face 模型缓存目录
+export HF_HOME=/data/yongxi/.cache/huggingface
+export HF_HUB_CACHE=/data/yongxi/.cache/huggingface
+
+# 运行训练
 python3 main.py --config=./exps/sdlora_c100.json  > C100.log 2>&1
 
 # python3 main.py --config=./exps/sdlora_inr.json  >> InR.log 2>&1 &
