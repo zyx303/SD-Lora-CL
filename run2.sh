acdb32a72fd7f9d4e2c8db3a6318b8387aa2348f
zyx303 <2091183827@qq.com>
2026-02-11 05:05:27 +0000

    script

--- run2.sh
+++ run2.sh
@@ -1,5 +1,5 @@
 # 设置 GPU 设备
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=2
 
 # 设置 Hugging Face 模型缓存目录
 export HF_HOME=/data/yongxi/.cache/huggingface
@@ -8,5 +8,5 @@ export HF_HUB_CACHE=/data/yongxi/.cache/huggingface
 # 运行训练
 # python3 main.py --config=./exps/sdlora_c100.json  > C100.log 2>&1
 
-python3 main.py --config=./exps/sdlora_inr_per_layer.json  > InR_per_layer.log 2>&1 
+python3 main.py --config=./exps/sdadapter_inr.json  > InR_sdadapter.log 2>&1 
 # python3 main.py --config=./exps/sdlora_ina.json  >> InA.log 2>&1 &
